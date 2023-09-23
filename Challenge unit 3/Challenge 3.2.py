6874a1a366f4f4999303b066f3f6f4788f56a398
90D3140881 <144828186+90D3140881@users.noreply.github.com>
2023-09-23 15:20:06 +0530

    Add files via upload

---
+++ Challenge unit 3/Challenge 3.2.py
@@ -0,0 +1,25 @@
+class student:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+def sort_students(student_list):
+  sorted_students = sorted(student_list,
+                          key=lambda student: student.cgpa,
+                          reverse=True)
+  return sorted_students
+
+
+students = [
+  student("Raveena", "001", 7.8),
+  student("Preetham", "002", 8.9),
+  student("yashika", "003", 9.1),
+  student("Yash", "004", 9.9),
+  ]
+
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+  print("Name: {}, Roll number: {}, CGPA: {}".format(student.name, student.roll_number, student.cgpa))
